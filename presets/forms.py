91b2268cbd58b08cf7dac6dfda9bae5e88c6ddb1
MonoSetton <maksymiliankloch@gmail.com>
2022-08-07 22:36:41 +0200

    Some mind clarity...

--- presets/forms.py
+++ presets/forms.py
@@ -1,10 +1,10 @@
-from django.forms import ModelForm, inlineformset_factory
+from django.forms import ModelForm
 from .models import Preset
 
 
 class PresetForm(ModelForm):
     class Meta:
         model = Preset
-        fields = '__all__'
+        fields = ['name']
 
         
--- presets/views.py
+++ presets/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect
 from django.forms import inlineformset_factory
+from .forms import PresetForm
 from .models import *
 
+
 def update_art(request, pk):
-    PresetFormSet = inlineformset_factory(Preset, Artifact, fields=('stat', 'status',), extra=4, can_delete=False, max_num=5)
+    PresetFormSet = inlineformset_factory(Preset, Artifact, fields=('main', 'first', 'second', 'third', 'fourth',
+                                                                    'status',), can_delete=False, max_num=1)
     preset = Preset.objects.get(id=pk)
     formset = PresetFormSet(instance=preset)
 
@@ -18,4 +21,18 @@ def update_art(request, pk):
 
 def details(request, pk):
     artifacts = Artifact.objects.all()
-    return render(request, 'presets\details.html', {'artifacts': artifacts})
+    preset = Preset.objects.all()
+    return render(request, 'presets\details.html', {'artifacts': artifacts, 'presets': preset})
+
+
+def create_preset(request):
+    if request.method == 'POST':
+        form = PresetForm(request.POST)
+        if form.is_valid():
+            preset = form.save(commit=False)
+            preset.author = request.user
+            form.save()
+            return render(request, 'presets\details.html')
+    else:
+        form = PresetForm()
+    return render(request, 'presets\create_preset.html', {'form': form})

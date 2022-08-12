90ffd23cd353196f56467e0a107fb748335d662a
MonoSetton <maksymiliankloch@gmail.com>
2022-08-12 13:54:27 +0200

    Tweaks

--- genshinpreset/settings.py
+++ genshinpreset/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e706uvy^j9*_e+%-@y9da17@!s^7_^kvn@szu=0b128$0am8^a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['genshin-presets.herokuapp.com', '127.0.0.1']
 
 
 # Application definition

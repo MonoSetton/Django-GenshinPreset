0ba8c0f7b27ad747028f5fc1a4f64cb616bbf4c3
MonoSetton <maksymiliankloch@gmail.com>
2022-08-14 11:02:53 +0200

    Security tweaks

--- presets/views.py
+++ presets/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render, redirect
 from django.forms import inlineformset_factory
 from .forms import PresetForm, PresetUpdateForm
 from .models import *
+from django.contrib.auth.decorators import login_required
 
 
+@login_required(login_url='/login')
 def update_art(request, pk):
     PresetFormSet = inlineformset_factory(Preset, Artifact, fields=('set', 'main', 'first', 'second', 'third', 'fourth',
                                                                     'status',), extra=5, can_delete=False, max_num=5)
@@ -23,6 +25,7 @@ def details(request, pk):
     return render(request, 'presets/details.html', {'artifacts': artifacts, 'preset': preset})
 
 
+@login_required(login_url='/login')
 def create_preset(request):
     if request.method == 'POST':
         form = PresetForm(request.POST)
@@ -36,6 +39,7 @@ def create_preset(request):
     return render(request, 'presets/create_preset.html', {'form': form})
 
 
+@login_required(login_url='/login')
 def delete_preset(request, pk):
     preset = Preset.objects.get(id=pk)
     if request.method == 'POST':
@@ -44,6 +48,7 @@ def delete_preset(request, pk):
     return render(request, 'presets/delete.html', {'preset': preset})
 
 
+@login_required(login_url='/login')
 def update_preset(request, pk):
     preset = Preset.objects.get(id=pk)
     form = PresetUpdateForm(instance=preset)

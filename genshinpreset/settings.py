341acaa1700decaf79dd1e685db677ce7234278c
MonoSetton <70628883+MonoSetton@users.noreply.github.com>
2022-12-30 12:46:40 +0100

    Update settings.py

--- genshinpreset/settings.py
+++ genshinpreset/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e706uvy^j9*_e+%-@y9da17@!s^7_^kvn@szu=0b128$0am8^a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['web-production-5f33.up.railway.app', '127.0.0.1']
 
